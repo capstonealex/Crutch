bc1fb15c230ee4fdf82776eec4ee13729218c8b7
Willcampbellmelb <w.campbell@student.unimelb.edu.au>
2020-02-24 17:55:38 +1100

    Jagged array and dictionary for screen built and tested

--- Pocket_Beagle_node/Pocket_Beagle/src/Crutch/Crutch.cpp
+++ Pocket_Beagle_node/Pocket_Beagle/src/Crutch/Crutch.cpp
@@ -4,6 +4,8 @@
 Crutch::Crutch(/* args */)
 {
     std::cout << "Crutch object created" << std::endl;
+    populate();
+
 }
 
 Crutch::~Crutch()
@@ -14,4 +16,30 @@ Crutch::~Crutch()
 void Crutch::run()
 {
     std::cout << "Crutch program running!" << std::endl;
+    std::string name = nextMotion[RIGHT_FORWARD][3];
+    std::cout << nextMotion[RIGHT_FORWARD][3]<< " : " << intLookupTable[name] << std::endl;
+}
+
+void Crutch::printVector(vector<vector<string>> const &mat)
+{
+    for (vector<string> row : mat)
+    {
+        for (string val : row)
+        {
+            cout << val << " ";
+        }
+        cout << '\n';
+    }
+}
+void Crutch::populate(){
+    intLookupTable["normal"] = 1;
+    intLookupTable["feet together"] = 2;
+    intLookupTable["backstep"] = 3;
+    intLookupTable["up stairs"] = 4;
+    intLookupTable["down stairs"] = 5;
+    intLookupTable["up slope"] = 6;
+    intLookupTable["down slope"] = 7;
+    intLookupTable["uneven"] = 8;
+    intLookupTable["Sit Down"] = 9;
+    intLookupTable["Stand Up"] = 10;
 }
--- Pocket_Beagle_node/Pocket_Beagle/src/Crutch/Crutch.h
+++ Pocket_Beagle_node/Pocket_Beagle/src/Crutch/Crutch.h
@@ -3,13 +3,33 @@
 *
 *
 */
+// VECTOR INDEX FOR nextMOTION lists
+#define LEFT_FORWARD 0
+#define RIGHT_FORWARD 1
+#define STANDING 2
+#define SITTING 3
 
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
 class Crutch
 {
 private:
     /* data */
+    vector<vector<string>> nextMotion{
+        {"normal", "feet together", "backstep", "up stairs", "down stairs", "up slope", "down slope", "uneven"},
+        {"normal", "feet together", "backstep", "up stairs", "down stairs", "up slope", "down slope", "uneven"},
+        {"Sit Down", "normal", "backstep", "up stairs", "down stairs", "up slope", "down slope", "uneven"},
+        {"Stand Up"}};
+
 public:
     Crutch(/* args */);
     ~Crutch();
     void run();
+    void initCrutch();
+    void printVector(vector<vector<string>> const &mat);
+    /*Look Up table to convert between nextMotion selections and OD int outputs to exo BBB*/
+    std::map<std::string, int> intLookupTable;
+    void populate();
 };
