4de4514c9933b1bde01f909e5abe0db2e14885d9
Willcampbellmelb <w.campbell@student.unimelb.edu.au>
2020-03-09 12:55:09 +1100

    Working cross comp - no static LCD linking

--- LCD/src/LiquidCrystal_I2C.h
+++ LCD/src/LiquidCrystal_I2C.h
@@ -48,21 +48,22 @@
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
-#define En 0b00000100  // Enable bit
-#define Rw 0b00000010  // Read/Write bit
-#define Rs 0b00000001  // Register select bit
+#define En 0b00000100 // Enable bit
+#define Rw 0b00000010 // Read/Write bit
+#define Rs 0b00000001 // Register select bit
 
 // flags for rt control
 #define SLEEP 0xff // NB: any 0xff sent will be treated as a skip/sleep if rtControl is on
 
-class LiquidCrystal_I2C{
+class LiquidCrystal_I2C
+{
 public:
-  LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows,uint8_t i2c_bus);
+  LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t i2c_bus);
   void configure_i2c_pins(char p1, char p2); // NB: sometimes this doesnt work and you'll have to configure manually
   int i2c_open(unsigned char bus, unsigned char addr);
   int i2c_close(int handle);
   int i2c_write_byte(int handle, unsigned char val);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
+  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
   void clear();
   void home();
   void noDisplay();
@@ -82,7 +83,7 @@ public:
   void noBacklight();
   void backlight();
   void autoscroll();
-  void noAutoscroll(); 
+  void noAutoscroll();
   void createChar(uint8_t, uint8_t[]);
   void setCursor(uint8_t col, uint8_t row);
   virtual void writeI2C(uint8_t);
@@ -96,25 +97,28 @@ public:
   int getHandle();
   int isQueueEmpty(); // returns 1 if queue is empty, 0 if it isn't
 
-////compatibility API function aliases
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
+  ////compatibility API function aliases
+  void blink_on();                                             // alias for blink()
+  void blink_off();                                            // alias for noBlink()
+  void cursor_on();                                            // alias for cursor()
+  void cursor_off();                                           // alias for noCursor()
+  void setBacklight(uint8_t new_val);                          // alias for backlight() and nobacklight()
+  void load_custom_character(uint8_t char_num, uint8_t *rows); // alias for createChar()
 
-////Unsupported API functions (not implemented in this library)
-uint8_t status();
-void setContrast(uint8_t new_val);
-uint8_t keypad();
-void setDelay(int,int);
-void on();
-void off();
-uint8_t init_bargraph(uint8_t graphtype);
-void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-	 
+  ////Unsupported API functions (not implemented in this library)
+  uint8_t status();
+  void setContrast(uint8_t new_val);
+  uint8_t keypad();
+  void setDelay(int, int);
+  void on();
+  void off();
+  uint8_t init_bargraph(uint8_t graphtype);
+  void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+  void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+  //made public for compulation
+  int _rtControl;
+  int _handle;
+  std::queue<uint8_t> commandQueue; // gives control over when each command is sent, only used when _rtControl == 1
 
 private:
   void init_priv();
@@ -131,9 +135,6 @@ private:
   uint8_t _rows;
   uint8_t _backlightval;
   uint8_t _i2c_bus;
-  int _handle;
-  int _rtControl;
-  std::queue<uint8_t> commandQueue; // gives control over when each command is sent, only used when _rtControl == 1
 };
 
 #endif
--- Pocket_Beagle_node/Pocket_Beagle/src/lib/LiquidCrystal_I2C.h
+++ Pocket_Beagle_node/Pocket_Beagle/src/lib/LiquidCrystal_I2C.h
@@ -4,6 +4,7 @@
 
 #include <inttypes.h>
 #include <string>
+#include <queue>
 
 // commands
 #define LCD_CLEARDISPLAY 0x01
@@ -47,18 +48,22 @@
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
-#define En 0b00000100  // Enable bit
-#define Rw 0b00000010  // Read/Write bit
-#define Rs 0b00000001  // Register select bit
+#define En 0b00000100 // Enable bit
+#define Rw 0b00000010 // Read/Write bit
+#define Rs 0b00000001 // Register select bit
 
-class LiquidCrystal_I2C{
+// flags for rt control
+#define SLEEP 0xff // NB: any 0xff sent will be treated as a skip/sleep if rtControl is on
+
+class LiquidCrystal_I2C
+{
 public:
-  LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows,uint8_t i2c_bus);
-  void configure_i2c_pins(char p1, char p2); // Used for BeagleBone Black or Pocket Beagle
+  LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t i2c_bus);
+  void configure_i2c_pins(char p1, char p2); // NB: sometimes this doesnt work and you'll have to configure manually
   int i2c_open(unsigned char bus, unsigned char addr);
   int i2c_close(int handle);
   int i2c_write_byte(int handle, unsigned char val);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
+  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
   void clear();
   void home();
   void noDisplay();
@@ -78,37 +83,42 @@ public:
   void noBacklight();
   void backlight();
   void autoscroll();
-  void noAutoscroll(); 
+  void noAutoscroll();
   void createChar(uint8_t, uint8_t[]);
-  void setCursor(uint8_t, uint8_t); 
+  void setCursor(uint8_t col, uint8_t row);
   virtual void writeI2C(uint8_t);
   void writeStr(std::string str);
- // using Print::write;
+  void rtControlOn();
+  void rtControlOff();
+  int sendNextCommand(); // sends next command from queue
 
   void command(uint8_t);
   void init();
   int getHandle();
+  int isQueueEmpty(); // returns 1 if queue is empty, 0 if it isn't
 
-////compatibility API function aliases
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
-void printstr(const char[]);
+  ////compatibility API function aliases
+  void blink_on();                                             // alias for blink()
+  void blink_off();                                            // alias for noBlink()
+  void cursor_on();                                            // alias for cursor()
+  void cursor_off();                                           // alias for noCursor()
+  void setBacklight(uint8_t new_val);                          // alias for backlight() and nobacklight()
+  void load_custom_character(uint8_t char_num, uint8_t *rows); // alias for createChar()
 
-////Unsupported API functions (not implemented in this library)
-uint8_t status();
-void setContrast(uint8_t new_val);
-uint8_t keypad();
-void setDelay(int,int);
-void on();
-void off();
-uint8_t init_bargraph(uint8_t graphtype);
-void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-	 
+  ////Unsupported API functions (not implemented in this library)
+  uint8_t status();
+  void setContrast(uint8_t new_val);
+  uint8_t keypad();
+  void setDelay(int, int);
+  void on();
+  void off();
+  uint8_t init_bargraph(uint8_t graphtype);
+  void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+  void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len, uint8_t pixel_col_end);
+  //made public for compulation
+  int _rtControl;
+  int _handle;
+  std::queue<uint8_t> commandQueue; // gives control over when each command is sent, only used when _rtControl == 1
 
 private:
   void init_priv();
@@ -125,7 +135,6 @@ private:
   uint8_t _rows;
   uint8_t _backlightval;
   uint8_t _i2c_bus;
-  int _handle;
 };
 
 #endif
